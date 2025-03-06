2cfa23a3eccd1daa994cda7c3ec74712ed9edeec
AttaliZ <korkrit.p@gmail.com>
2025-03-07 00:06:52 +0700

    Save changes before pulling

---
+++ src/Board.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Square from './Square';
+
+const Board = ({ squares, onClick }) => {
+  const renderSquare = (i) => {
+    return (
+      <Square
+        value={squares[i]}
+        onClick={() => onClick(i)}
+      />
+    );
+  };
+
+  return (
+    <div className="board">
+      {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((i) => renderSquare(i))}
+    </div>
+  );
+};
+
+export default Board;
